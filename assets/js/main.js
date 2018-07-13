1b08247ad41355957cb8a9ff33a39b8e6f57fe25
Stewart Viera <stviera@microsoft.com>
2018-07-12 18:06:09 -0700

    Changes to demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,299 +1,390 @@
 /*
-	Multiverse by HTML5 UP
+	Parallelism by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
-	});
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$main = $('#main'),
+		settings = {
 
-	$(function() {
+			// Keyboard shortcuts.
+				keyboardShortcuts: {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#wrapper');
+					// If true, enables scrolling via keyboard shortcuts.
+						enabled: true,
 
-		// Hack: Enable IE workarounds.
-			if (skel.vars.IEVersion < 12)
-				$body.addClass('ie');
+					// Sets the distance to scroll when using the left/right arrow keys.
+						distance: 50
 
-		// Touch?
-			if (skel.vars.mobile)
-				$body.addClass('touch');
+				},
 
-		// Transitions supported?
-			if (skel.canUse('transition')) {
+			// Scroll wheel.
+				scrollWheel: {
 
-				// Add (and later, on load, remove) "loading" class.
-					$body.addClass('loading');
+					// If true, enables scrolling via the scroll wheel.
+						enabled: true,
 
-					$window.on('load', function() {
-						window.setTimeout(function() {
-							$body.removeClass('loading');
-						}, 100);
-					});
+					// Sets the scroll wheel factor. (Ideally) a value between 0 and 1 (lower = slower scroll, higher = faster scroll).
+						factor: 1
 
-				// Prevent transitions/animations on resize.
-					var resizeTimeout;
+				},
 
-					$window.on('resize', function() {
+			// Scroll zones.
+				scrollZones: {
 
-						window.clearTimeout(resizeTimeout);
+					// If true, enables scrolling via scroll zones on the left/right edges of the scren.
+						enabled: true,
 
-						$body.addClass('resizing');
+					// Sets the speed at which the page scrolls when a scroll zone is active (higher = faster scroll, lower = slower scroll).
+						speed: 15
 
-						resizeTimeout = window.setTimeout(function() {
-							$body.removeClass('resizing');
-						}, 100);
+				}
 
-					});
+		};
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '481px',   '736px'  ],
+			xsmall:  [ null,      '480px'  ],
+		});
+
+	// Tweaks/fixes.
+
+		// Mobile: Revert to native scrolling.
+			if (browser.mobile) {
+
+				// Disable all scroll-assist features.
+					settings.keyboardShortcuts.enabled = false;
+					settings.scrollWheel.enabled = false;
+					settings.scrollZones.enabled = false;
+
+				// Re-enable overflow on main.
+					$main.css('overflow-x', 'auto');
 
 			}
 
-		// Scroll back to top.
-			$window.scrollTop(0);
+		// IE: Fix min-height/flexbox.
+			if (browser.name == 'ie')
+				$wrapper.css('height', '100vh');
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		// iOS: Compensate for address bar.
+			if (browser.os == 'ios')
+				$wrapper.css('min-height', 'calc(100vh - 30px)');
 
-		// Panels.
-			var $panels = $('.panel');
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-			$panels.each(function() {
+	// Items.
 
-				var $this = $(this),
-					$toggles = $('[href="#' + $this.attr('id') + '"]'),
-					$closer = $('<div class="closer" />').appendTo($this);
-
-				// Closer.
-					$closer
-						.on('click', function(event) {
-							$this.trigger('---hide');
-						});
-
-				// Events.
-					$this
-						.on('click', function(event) {
-							event.stopPropagation();
-						})
-						.on('---toggle', function() {
-
-							if ($this.hasClass('active'))
-								$this.triggerHandler('---hide');
-							else
-								$this.triggerHandler('---show');
-
-						})
-						.on('---show', function() {
-
-							// Hide other content.
-								if ($body.hasClass('content-active'))
-									$panels.trigger('---hide');
-
-							// Activate content, toggles.
-								$this.addClass('active');
-								$toggles.addClass('active');
-
-							// Activate body.
-								$body.addClass('content-active');
-
-						})
-						.on('---hide', function() {
-
-							// Deactivate content, toggles.
-								$this.removeClass('active');
-								$toggles.removeClass('active');
-
-							// Deactivate body.
-								$body.removeClass('content-active');
-
-						});
-
-				// Toggles.
-					$toggles
-						.removeAttr('href')
-						.css('cursor', 'pointer')
-						.on('click', function(event) {
-
-							event.preventDefault();
-							event.stopPropagation();
-
-							$this.trigger('---toggle');
-
-						});
-
+		// Assign a random "delay" class to each thumbnail item.
+			$('.item.thumb').each(function() {
+				$(this).addClass('delay-' + Math.floor((Math.random() * 6) + 1));
 			});
 
-			// Global events.
-				$body
-					.on('click', function(event) {
+		// IE: Fix thumbnail images.
+			if (browser.name == 'ie')
+				$('.item.thumb').each(function() {
 
-						if ($body.hasClass('content-active')) {
+					var $this = $(this),
+						$img = $this.find('img');
 
-							event.preventDefault();
-							event.stopPropagation();
+					$this
+						.css('background-image', 'url(' + $img.attr('src') + ')')
+						.css('background-size', 'cover')
+						.css('background-position', 'center');
 
-							$panels.trigger('---hide');
+					$img
+						.css('opacity', '0');
 
-						}
+				});
 
-					});
+	// Poptrox.
+		$main.poptrox({
+			onPopupOpen: function() { $body.addClass('is-poptrox-visible'); },
+			onPopupClose: function() { $body.removeClass('is-poptrox-visible'); },
+			overlayColor: '#1a1f2c',
+			overlayOpacity: 0.75,
+			popupCloserText: '',
+			popupLoaderText: '',
+			selector: '.item.thumb a.image',
+			caption: function($a) {
+				return $a.prev('h2').html();
+			},
+			usePopupDefaultStyling: false,
+			usePopupCloser: false,
+			usePopupCaption: true,
+			usePopupNav: true,
+			windowMargin: 50
+		});
+
+		breakpoints.on('>small', function() {
+			$main[0]._poptrox.windowMargin = 50;
+		});
+
+		breakpoints.on('<=small', function() {
+			$main[0]._poptrox.windowMargin = 0;
+		});
+
+	// Keyboard shortcuts.
+		if (settings.keyboardShortcuts.enabled)
+			(function() {
 
 				$window
-					.on('keyup', function(event) {
 
-						if (event.keyCode == 27
-						&&	$body.hasClass('content-active')) {
+					// Keypress event.
+						.on('keydown', function(event) {
 
+							var scrolled = false;
+
+							if ($body.hasClass('is-poptrox-visible'))
+								return;
+
+							switch (event.keyCode) {
+
+								// Left arrow.
+									case 37:
+										$main.scrollLeft($main.scrollLeft() - settings.keyboardShortcuts.distance);
+										scrolled = true;
+										break;
+
+								// Right arrow.
+									case 39:
+										$main.scrollLeft($main.scrollLeft() + settings.keyboardShortcuts.distance);
+										scrolled = true;
+										break;
+
+								// Page Up.
+									case 33:
+										$main.scrollLeft($main.scrollLeft() - $window.width() + 100);
+										scrolled = true;
+										break;
+
+								// Page Down, Space.
+									case 34:
+									case 32:
+										$main.scrollLeft($main.scrollLeft() + $window.width() - 100);
+										scrolled = true;
+										break;
+
+								// Home.
+									case 36:
+										$main.scrollLeft(0);
+										scrolled = true;
+										break;
+
+								// End.
+									case 35:
+										$main.scrollLeft($main.width());
+										scrolled = true;
+										break;
+
+							}
+
+							// Scrolled?
+								if (scrolled) {
+
+									// Prevent default.
+										event.preventDefault();
+										event.stopPropagation();
+
+									// Stop link scroll.
+										$main.stop();
+
+								}
+
+						});
+
+			})();
+
+	// Scroll wheel.
+		if (settings.scrollWheel.enabled)
+			(function() {
+
+				// Based on code by @miorel + @pieterv of Facebook (thanks guys :)
+				// github.com/facebook/fixed-data-table/blob/master/src/vendor_upstream/dom/normalizeWheel.js
+					var normalizeWheel = function(event) {
+
+						var	pixelStep = 10,
+							lineHeight = 40,
+							pageHeight = 800,
+							sX = 0,
+							sY = 0,
+							pX = 0,
+							pY = 0;
+
+						// Legacy.
+							if ('detail' in event)
+								sY = event.detail;
+							else if ('wheelDelta' in event)
+								sY = event.wheelDelta / -120;
+							else if ('wheelDeltaY' in event)
+								sY = event.wheelDeltaY / -120;
+
+							if ('wheelDeltaX' in event)
+								sX = event.wheelDeltaX / -120;
+
+						// Side scrolling on FF with DOMMouseScroll.
+							if ('axis' in event
+							&&	event.axis === event.HORIZONTAL_AXIS) {
+								sX = sY;
+								sY = 0;
+							}
+
+						// Calculate.
+							pX = sX * pixelStep;
+							pY = sY * pixelStep;
+
+							if ('deltaY' in event)
+								pY = event.deltaY;
+
+							if ('deltaX' in event)
+								pX = event.deltaX;
+
+							if ((pX || pY)
+							&&	event.deltaMode) {
+
+								if (event.deltaMode == 1) {
+									pX *= lineHeight;
+									pY *= lineHeight;
+								}
+								else {
+									pX *= pageHeight;
+									pY *= pageHeight;
+								}
+
+							}
+
+						// Fallback if spin cannot be determined.
+							if (pX && !sX)
+								sX = (pX < 1) ? -1 : 1;
+
+							if (pY && !sY)
+								sY = (pY < 1) ? -1 : 1;
+
+						// Return.
+							return {
+								spinX: sX,
+								spinY: sY,
+								pixelX: pX,
+								pixelY: pY
+							};
+
+					};
+
+				// Wheel event.
+					$body.on('wheel', function(event) {
+
+						// Disable on <=small.
+							if (breakpoints.active('<=small'))
+								return;
+
+						// Prevent default.
 							event.preventDefault();
 							event.stopPropagation();
 
-							$panels.trigger('---hide');
+						// Stop link scroll.
+							$main.stop();
 
-						}
+						// Calculate delta, direction.
+							var	n = normalizeWheel(event.originalEvent),
+								x = (n.pixelX != 0 ? n.pixelX : n.pixelY),
+								delta = Math.min(Math.abs(x), 150) * settings.scrollWheel.factor,
+								direction = x > 0 ? 1 : -1;
+
+						// Scroll page.
+							$main.scrollLeft($main.scrollLeft() + (delta * direction));
 
 					});
 
-		// Header.
-			var $header = $('#header');
+			})();
 
-			// Links.
-				$header.find('a').each(function() {
+	// Scroll zones.
+		if (settings.scrollZones.enabled)
+			(function() {
 
-					var $this = $(this),
-						href = $this.attr('href');
+				var	$left = $('<div class="scrollZone left"></div>'),
+					$right = $('<div class="scrollZone right"></div>'),
+					$zones = $left.add($right),
+					paused = false,
+					intervalId = null,
+					direction,
+					activate = function(d) {
 
-					// Internal link? Skip.
-						if (!href
-						||	href.charAt(0) == '#')
-							return;
+						// Disable on <=small.
+							if (breakpoints.active('<=small'))
+								return;
 
-					// Redirect on click.
-						$this
-							.removeAttr('href')
-							.css('cursor', 'pointer')
-							.on('click', function(event) {
+						// Paused? Bail.
+							if (paused)
+								return;
 
-								event.preventDefault();
-								event.stopPropagation();
+						// Stop link scroll.
+							$main.stop();
 
-								window.location.href = href;
+						// Set direction.
+							direction = d;
 
-							});
+						// Initialize interval.
+							clearInterval(intervalId);
 
-				});
-
-		// Footer.
-			var $footer = $('#footer');
-
-			// Copyright.
-			// This basically just moves the copyright line to the end of the *last* sibling of its current parent
-			// when the "medium" breakpoint activates, and moves it back when it deactivates.
-				$footer.find('.copyright').each(function() {
-
-					var $this = $(this),
-						$parent = $this.parent(),
-						$lastParent = $parent.parent().children().last();
-
-					skel
-						.on('+medium', function() {
-							$this.appendTo($lastParent);
-						})
-						.on('-medium', function() {
-							$this.appendTo($parent);
-						});
-
-				});
-
-		// Main.
-			var $main = $('#main');
-
-			// Thumbs.
-				$main.children('.thumb').each(function() {
-
-					var	$this = $(this),
-						$image = $this.find('.image'), $image_img = $image.children('img'),
-						x;
-
-					// No image? Bail.
-						if ($image.length == 0)
-							return;
-
-					// Image.
-					// This sets the background of the "image" <span> to the image pointed to by its child
-					// <img> (which is then hidden). Gives us way more flexibility.
-
-						// Set background.
-							$image.css('background-image', 'url(' + $image_img.attr('src') + ')');
-
-						// Set background position.
-							if (x = $image_img.data('position'))
-								$image.css('background-position', x);
-
-						// Hide original img.
-							$image_img.hide();
-
-					// Hack: IE<11 doesn't support pointer-events, which means clicks to our image never
-					// land as they're blocked by the thumbnail's caption overlay gradient. This just forces
-					// the click through to the image.
-						if (skel.vars.IEVersion < 11)
-							$this
-								.css('cursor', 'pointer')
-								.on('click', function() {
-									$image.trigger('click');
-								});
-
-				});
-
-			// Poptrox.
-				$main.poptrox({
-					baseZIndex: 20000,
-					caption: function($a) {
-
-						var s = '';
-
-						$a.nextAll().each(function() {
-							s += this.outerHTML;
-						});
-
-						return s;
+							intervalId = setInterval(function() {
+								$main.scrollLeft($main.scrollLeft() + (settings.scrollZones.speed * direction));
+							}, 25);
 
 					},
-					fadeSpeed: 300,
-					onPopupClose: function() { $body.removeClass('modal-active'); },
-					onPopupOpen: function() { $body.addClass('modal-active'); },
-					overlayOpacity: 0,
-					popupCloserText: '',
-					popupHeight: 150,
-					popupLoaderText: '',
-					popupSpeed: 300,
-					popupWidth: 150,
-					selector: '.thumb > a.image',
-					usePopupCaption: true,
-					usePopupCloser: true,
-					usePopupDefaultStyling: false,
-					usePopupForceClose: true,
-					usePopupLoader: true,
-					usePopupNav: true,
-					windowMargin: 50
-				});
+					deactivate = function() {
 
-				// Hack: Set margins to 0 when 'xsmall' activates.
-					skel
-						.on('-xsmall', function() {
-							$main[0]._poptrox.windowMargin = 50;
-						})
-						.on('+xsmall', function() {
-							$main[0]._poptrox.windowMargin = 0;
-						});
+						// Unpause.
+							paused = false;
 
-	});
+						// Clear interval.
+							clearInterval(intervalId);
+
+					};
+
+				$zones
+					.appendTo($wrapper)
+					.on('mouseleave mousedown', function(event) {
+						deactivate();
+					});
+
+				$left
+					.css('left', '0')
+					.on('mouseenter', function(event) {
+						activate(-1);
+					});
+
+				$right
+					.css('right', '0')
+					.on('mouseenter', function(event) {
+						activate(1);
+					});
+
+				$body
+					.on('---pauseScrollZone', function(event) {
+
+						// Pause.
+							paused = true;
+
+						// Unpause after delay.
+							setTimeout(function() {
+								paused = false;
+							}, 500);
+
+					});
+
+			})();
 
 })(jQuery);
